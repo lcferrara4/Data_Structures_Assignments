b813e38de3f7e95b24e06196b292bcd590055458
Lauren Christine Ferrara <lferrara@student00.cse.nd.edu>
2016-09-28 19:12:31 -0400

    fixed problem of checking nodes later on to previous nodes with min and max approach

--- challenge05/solution.cpp
+++ challenge05/solution.cpp
@@ -1,10 +1,11 @@
 // Challenge 05: BST
 #include <iostream>
 #include <vector>
+#include <stddef.h>
 
 using namespace std;
 
-bool checkChildren( vector<int>, int, bool );
+bool checkChildren( vector<int>, int, bool, int, int );
 
 // Main Execution
 
@@ -28,7 +29,7 @@ int main(int argc, char *argv[]) {
         }
         
         // check tree
-        bst = checkChildren(tree, 0, bst);
+        bst = checkChildren(tree, 0, bst, 0, 0);
 
         // print result
         if ( bst ){
@@ -42,17 +43,22 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-bool checkChildren( vector<int> tree, int curr, bool bst ){
+bool checkChildren( vector<int> tree, int curr, bool bst, int min, int max ){
+
 
     // get indices of left and right children
     int left = 2 * curr + 1;
     int right = 2 * curr + 2;
 
+    if ((min != 0 && tree[curr] <= min) || (max != 0 && tree[curr] > max)){
+        return 0;
+    }
+
     // check left child
     if( left >= tree.size() ){  // reached end of tree
         return 1;
     } else if( tree[left] <= tree[curr] && tree[left] != -1 ){
-        bst = bst * checkChildren( tree, left, bst );
+        bst = bst * checkChildren( tree, left, bst, min, tree[curr] );
     } else if( tree[left] != -1 ){  // violates bst rule
         return 0;
     }
@@ -61,7 +67,7 @@ bool checkChildren( vector<int> tree, int curr, bool bst ){
     if( right >= tree.size() ){ // reached end of tree
         return 1;
     } else if( tree[right] > tree[curr] ){
-        bst = bst * checkChildren( tree, right, bst );
+        bst = bst * checkChildren( tree, right, bst, tree[curr], max );
     } else if( tree[right] != -1 ){ // violates bst rule
         return 0;
     }
